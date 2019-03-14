24427a5f1fed85e70e85e17275597f35dddcae4d
MiquelMR <miquel.martinez@colba.es>
2019-03-14 20:38:44 +0100

    on it

--- app/src/main/java/com/simplemvpexample/app/screens/ListOfNotesScreen.java
+++ app/src/main/java/com/simplemvpexample/app/screens/ListOfNotesScreen.java
@@ -1,13 +1,15 @@
-package com.simplemvpexample.app;
+package com.simplemvpexample.app.screens;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.simplemvpexample.app.R;
+
+public class ListOfNotesScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView( R.layout.list_of_notes_screen );
     }
 }
---
+++ app/src/main/java/com/simplemvpexample/app/screens/NoteDetailScreen.java
@@ -0,0 +1,15 @@
+package com.simplemvpexample.app.screens;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.simplemvpexample.app.R;
+
+public class NoteDetailScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_note_screen );
+    }
+}
