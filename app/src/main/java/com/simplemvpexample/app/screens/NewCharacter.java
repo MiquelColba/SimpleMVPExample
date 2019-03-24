88699e780e9500ffb5ba8425374d6a46d035d6e4
MiquelMR <miquel.martinez@colba.es>
2019-03-24 11:50:10 +0100

    all-in-one finished

--- app/src/main/java/com/simplemvpexample/app/screens/NewCharacter.java
+++ app/src/main/java/com/simplemvpexample/app/screens/NewCharacter.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -154,6 +155,21 @@ public class NewCharacter extends AppCompatActivity implements TextView.OnEditor
 
     }
 
+    public String getRealPathFromURI(Context context, Uri contentUri) {
+        Cursor cursor = null;
+        try {
+            String[] proj = { MediaStore.Images.Media.DATA };
+            cursor = context.getContentResolver().query(contentUri,  proj, null, null, null);
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult( requestCode, resultCode, data );
@@ -161,7 +177,11 @@ public class NewCharacter extends AppCompatActivity implements TextView.OnEditor
         if (requestCode == IMAGE_SELECTION && resultCode == RESULT_OK) {
 
             if ( data != null ) {
-                setImagePicture( data.getData() );
+
+                String path = getRealPathFromURI( this, data.getData() );
+                Uri imageUri = Uri.fromFile( new File( path ) );
+                setImagePicture( imageUri );
+
             } else if (currentPhotoPath != null) {
 
                 galleryAddPic();
